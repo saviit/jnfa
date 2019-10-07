6d13c01e29bd9c1bd177f6358781e4fc174ace7d
Sami Viitanen <sami.a.viitanen@outlook.com>
2019-10-07 19:33:50 +0300

    rename get(String)->getByName(String)

--- SortedStateSet.java
+++ SortedStateSet.java
@@ -1,14 +1,20 @@
 import java.util.Iterator;
 import java.util.SortedSet;
 
+/**
+ * SortedSet used to store NFA states.
+ */
 public class SortedStateSet implements SortedSet {
     
     public SortedStateSet() {
         super();
     }
 
-    // Retrieves the unique state in this set identified by its name
-    public NFAState get(String name) {
+    /** 
+     * Retrieves the unique state in this set identified by its name
+     * @return the specified NFAState, or null if not found
+    */
+    public NFAState getByName(String name) {
         Iterator it = this.iterator();
         while (it.hasNext()) {
             NFAState state = it.next();
