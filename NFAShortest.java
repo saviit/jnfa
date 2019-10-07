b0c85939f48e19d259f22c78cf394b820b32f9d2
Sami Viitanen <sami.a.viitanen@outlook.com>
2019-10-07 22:05:51 +0300

    reflect changes in dependencies

--- NFAShortest.java
+++ NFAShortest.java
@@ -1,11 +1,10 @@
-import java.util.SortedSet;
 import java.util.ArrayList;
 
 public class NFAShortest {
 
     public static void main(String[] args) {
         // create a set of states
-        SortedStateSet<NFAState> states = new SortedStateSet<NFAState>() { };
+        NFAStateSet states = new NFAStateSet();
         for (int i = 1; i < 16; i++) {
             if (i == 10 || i == 15) {
                 states.add(new NFAState("" + i, true));
@@ -14,31 +13,29 @@ public class NFAShortest {
             }
         }
         // create alphabet
-        SortedSet<Character> alphabet = new SortedSet<Character>() { };
-        ArrayList<Character> c = {'a', 'b', 'c', 'd', 'o', 'p', 's', 't', 'u'};
-        alphabet.addAll(c);
-
-        
+        Character carr[] = {'a', 'b', 'c', 'd', 'o', 'p', 's', 't', 'u'};
+        Alphabet alphabet = new Alphabet(carr);
+                
         // create arcs/paths
         TRel tr = new TRel();
-        tr.add(new TFunc(states.getByName("1"), states.getByName("2"), 'a'));
-        tr.add(new TFunc(states.getByName("1"), states.getByName("3"), 'b'));
-        tr.add(new TFunc(states.getByName("1"), states.getByName("4"), 'c'));
-        tr.add(new TFunc(states.getByName("2"), states.getByName("5"), 'u'));
-        tr.add(new TFunc(states.getByName("2"), states.getByName("6"), 'd'));
-        tr.add(new TFunc(states.getByName("3"), states.getByName("6"), 'c'));
-        tr.add(new TFunc(states.getByName("5"), states.getByName("7"), 't'));
-        tr.add(new TFunc(states.getByName("6"), states.getByName("8"), 'p'));
-        tr.add(new TFunc(states.getByName("6"), states.getByName("9"), 's'));
-        tr.add(new TFunc(states.getByName("7"), states.getByName("10"), 'o'));
-        tr.add(new TFunc(states.getByName("8"), states.getByName("11"), 't'));
-        tr.add(new TFunc(states.getByName("9"), states.getByName("12"), 'a'));
-        tr.add(new TFunc(states.getByName("9"), states.getByName("13"), 'p'));
-        tr.add(new TFunc(states.getByName("12"), states.getByName("14"), 't'));
-        tr.add(new TFunc(states.getByName("13"), states.getByName("15"), 'o'));
+        tr.add(new TFunc(states.get("1"), states.get("2"), 'a'));
+        tr.add(new TFunc(states.get("1"), states.get("3"), 'b'));
+        tr.add(new TFunc(states.get("1"), states.get("4"), 'c'));
+        tr.add(new TFunc(states.get("2"), states.get("5"), 'u'));
+        tr.add(new TFunc(states.get("2"), states.get("6"), 'd'));
+        tr.add(new TFunc(states.get("3"), states.get("6"), 'c'));
+        tr.add(new TFunc(states.get("5"), states.get("7"), 't'));
+        tr.add(new TFunc(states.get("6"), states.get("8"), 'p'));
+        tr.add(new TFunc(states.get("6"), states.get("9"), 's'));
+        tr.add(new TFunc(states.get("7"), states.get("10"), 'o'));
+        tr.add(new TFunc(states.get("8"), states.get("11"), 't'));
+        tr.add(new TFunc(states.get("9"), states.get("12"), 'a'));
+        tr.add(new TFunc(states.get("9"), states.get("13"), 'p'));
+        tr.add(new TFunc(states.get("12"), states.get("14"), 't'));
+        tr.add(new TFunc(states.get("13"), states.get("15"), 'o'));
 
         // initialize NFA
-        NFA nfa = new NFA(states, alphabet, tr, states.getByName("1"));
+        NFA nfa = new NFA(states, alphabet, tr, states.get("1"));
         
         int shortest = nfa.shortest2();
         System.out.println(String.format("Shortest accepted language was %d characters long.", shortest));
