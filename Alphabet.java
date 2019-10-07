c09350cd34ced55c854530f1973fde01541b9511
Sami Viitanen <sami.a.viitanen@outlook.com>
2019-10-07 22:39:28 +0300

    convert to using Character instead of char

--- Alphabet.java
+++ Alphabet.java
@@ -5,84 +5,88 @@ import java.util.NoSuchElementException;
 /**
  * Simple collection class to store the alphabet for an NFA
  */
-public class Alphabet implements Iterable<Char> {
-    private ArrayList<Char> c;
+public class Alphabet implements Iterable<Character> {
+    private ArrayList<Character> c;
 
-    public Alphabet() { c = new ArrayList<Char>(0); }
+    public Alphabet() { c = new ArrayList<Character>(0); }
     public Alphabet(char[] chars) {
-        c = new ArrayList<Char>(chars.length);
+        c = new ArrayList<Character>(chars.length);
         addAll(chars);
     }
     public Alphabet(Character[] chars) {
-        c = new ArrayList<Char>(chars.length);
+        c = new ArrayList<Character>(chars.length);
         addAll(chars);
     }
-    public Alphabet(Collection<Char> chars) {
-        c = new ArrayList<Char>(chars.size());
+    public Alphabet(Collection<Character> chars) {
+        c = new ArrayList<Character>(chars.size());
         addAll(chars);
     }
 
     public void add(char c) {
-        if (!contains(c)) {
-            this.c.add(c);
+        Character cc = c;
+        if (!contains(cc)) {
+            this.c.add(cc);
         }
     }
 
     public void add(Character c) {
         if (!contains(c)) {
-            this.c.add(c.charValue());
+            this.c.add(c);
         }
     }
 
     public void addAll(char[] c) {
+        Character cc;
         for (int i = 0; i < c.length; i++) {
-            this.add(c[i]);
+            cc = c[i];
+            add(cc);
         }
     }
     public void addAll(Character[] c) {
         for (int i = 0; i < c.length; i++) {
-            this.add(c[i]);
+            add(c[i]);
         }
     }
-    public void addAll(Collection<Char> c) {
-        Iterator<Char> it = c.iterator();
+    public void addAll(Collection<Character> c) {
+        Iterator<Character> it = c.iterator();
         while (it.hasNext()) {
-            char ch = it.next();
-            this.add(ch);
+            Character ch = it.next();
+            add(ch);
         }
     }
 
     public boolean contains(char c) {
-        return this.c.contains(c);
+        return contains(new Character(c));
     }
     public boolean contains(Character c) {
-        return this.c.contains(c.charValue());
+        return this.c.contains(c);
     }
 
-    public Iterator<Char> iterator() {
+    public Iterator<Character> iterator() {
         return c.iterator();
     }
 
-    public char get(int index) {
+    public Character get(int index) {
         if (index < c.size()) {
             return c.get(index);
         } else throw new IndexOutOfBoundsException("Requested index was out of bounds: " + index);
     }
 
-    public int indexOf(char c) {
+    public int indexOf(Character c) {
         if (contains(c)) return this.c.indexOf(c);
         else throw new NoSuchElementException("Could not find element: " + c);
     }
 
     public void remove(char c) {
-        if (contains(c)) {
-            this.c.remove(c);
+        Character cc = c;
+        if (contains(cc)) {
+            remove(cc);
         }
     }
 
     public void remove(Character c) {
         if (contains(c)) {
-            this.c.remove(c.charValue());
+            this.c.remove(c);
         }
     }
 
