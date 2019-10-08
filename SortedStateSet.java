a77d492d7f7fa7ac370f9bc55bc317abfae4ed23
Sami Viitanen <sami.a.viitanen@outlook.com>
2019-10-08 21:10:06 +0300

    -

--- SortedStateSet.java
+++ SortedStateSet.java
@@ -1,10 +1,11 @@
 import java.util.Iterator;
 import java.util.SortedSet;
+import java.lang.ClassCastException;
 
 /**
  * SortedSet used to store NFA states.
  */
-public abstract class SortedStateSet<E> implements SortedSet {
+public class SortedStateSet<NFAState> implements SortedSet {
     
     public SortedStateSet() {
         super();
@@ -14,14 +15,30 @@ public abstract class SortedStateSet<E> implements SortedSet {
      * Retrieves the unique state in this set identified by its name
      * @return the specified NFAState, or null if not found
     */
-    public NFAState getByName(String name) {
+    public NFAState getByName(String name) throws ClassCastException {
         Iterator it = this.iterator();
         while (it.hasNext()) {
-            NFAState state = it.next();
+            NFAState state;
+            Object o = it.next();
+            if (!o.getClass().getName().equals(NFAState.class.getName())) {
+                throw new ClassCastException("Requested object was not of compatible type.");
+            } else {
+                state = (NFAState)o;
+            }
+            
             if (state.name.equals(name)) return state;
         }
         return null;
     }
 
+    @Override
+    public NFAState last() {
+        return null;
+    }
+    @Override
+    public NFAState first() {
+        return null;
+    }
+    
     
 }
